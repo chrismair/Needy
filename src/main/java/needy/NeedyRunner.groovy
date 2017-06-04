fdbd53acd862855574278b2c0ccef29567b8a274
Chris Mair <chrismair@sourceforge.net>
2017-06-04 13:31:38 -0400

    NeedyRunner (initial)

---
+++ src/main/java/needy/NeedyRunner.groovy
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2017 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package needy
+
+import java.util.List
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+class NeedyRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(NeedyRunner)
+	
+	ApplicationBuildSet applicationBuildSet
+	
+	void execute() {
+		assert applicationBuildSet
+		
+		DependencyParser dependencyParser = new GroovyDslGradleDependencyParser()
+		def applicationBuilds = applicationBuildSet.getApplicationBuilds()
+		LOG.info("applicationBuilds=" + applicationBuilds)
+		applicationBuilds.forEach{ ApplicationBuild applicationBuild ->
+			LOG.info("Processing application [${applicationBuild.name}]")
+			applicationBuild.buildScripts.each { BuildScript buildScript ->
+				String buildFileText = buildScript.getText()
+				List<Dependency> dependencies = dependencyParser.parse(buildFileText)
+				LOG.info("    dependencies=$dependencies")
+			}
+		}
+
+	}
+	
+}
---
+++ src/test/java/needy/NeedyRunnerTest.groovy
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2017 the original author or authors.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package needy
+
+import org.junit.Test
+
+class NeedyRunnerTest extends AbstractTestCase {
+
+	private static final String TEXT = "abc123"
+	
+	private NeedyRunner needyRunner = new NeedyRunner()
+	
+	@Test
+	void test_execute_RequiredPropertiesNotInitialized() {
+		shouldFailWithMessage("applicationBuildSet") { needyRunner.execute() }
+	}
+	
+	@Test
+	void test_execute() {
+		final TEXT = """
+			needy {
+				Sample1("file:src/test/resources/sample1-build.gradle")
+				Sample_Two("file:src/test/resources/sample2-build.gradle")
+			}
+		"""
+		def applicationBuildSet = GroovyDslApplicationBuildSet.fromString(TEXT)
+		def applicationBuilds = applicationBuildSet.getApplicationBuilds()
+		log(applicationBuilds)
+		needyRunner.applicationBuildSet = applicationBuildSet
+		needyRunner.execute()
+		
+	}
+	
+}
