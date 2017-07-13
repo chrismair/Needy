8dd0737ea7a1c0fbdb8a3f414b0c83814006c19b
Chris Mair <chrismair@sourceforge.net>
2017-07-12 20:16:03 -0400

    GrailsBuildConfigDependencyParser - initial

--- src/test/java/org/dx42/needy/parser/GrailsBuildConfigDependencyParserTest.groovy
+++ src/test/java/org/dx42/needy/parser/GrailsBuildConfigDependencyParserTest.groovy
@@ -107,7 +107,7 @@ class GrailsBuildConfigDependencyParserTest extends AbstractTestCase {
 		assert parser.parse(NAME, SOURCE) == [new Dependency(applicationName:NAME, configuration:"runtime", group:null, name:"MyUtil", version:"9")]
 	}
 
-	// TODO Is name-only valid in "BuildConfig.groovy"
+	// TODO Is name-only valid in "BuildConfig.groovy"?
 	@Test
 	void test_parse_StringFormat_NameOnly() {
 		final SOURCE = """grails.project.dependency.resolution = {
@@ -147,179 +147,32 @@ class GrailsBuildConfigDependencyParserTest extends AbstractTestCase {
 	}
 
 	@Test
-	void test_parse_FullGradleBuildFile() {
-		String source = new File('src/test/resources/sample1-grails-buildconfig.groovy').text
+	void test_parse_FullGrailsBuildConfigFile() {
+		String source = new File('src/test/resources/sample1-grails-buildconfig.txt').text
 		def dependencies = parser.parse(NAME, source)
 		assert dependencies.size() == 11
+		assert dependencies[0] == new Dependency(applicationName:"MyApp1", group:"mydb", name:"client", version:"16.0.EBF26086", configuration:"runtime") 
+		assert dependencies[1] == new Dependency(applicationName:"MyApp1", group:"acme", name:"util", version:"1.0", configuration:"runtime") 
+		assert dependencies[2] == new Dependency(applicationName:"MyApp1", group:"commons-dbcp", name:"commons-dbcp", version:"1.4", configuration:"build") 
+		assert dependencies[3] == new Dependency(applicationName:"MyApp1", group:"acme", name:"architecture", version:"3.29.0", configuration:"compile")
+		assert dependencies[4] == new Dependency(applicationName:"MyApp1", group:"commons-collections", name:"commons-collections", version:"3.2.2", configuration:"compile") 
+		assert dependencies[5] == new Dependency(applicationName:"MyApp1", group:"javax.validation", name:"validation-api", version:"1.1.0.Final", configuration:"compile")
+		assert dependencies[6] == new Dependency(applicationName:"MyApp1", group:"org.springframework", name:"spring-orm", version:"?", configuration:"compile")
+		assert dependencies[7] == new Dependency(applicationName:"MyApp1", group:"org.springframework", name:"spring-aop", version:"4.0.5.RELEASE", configuration:"compile") 
+		assert dependencies[8] == new Dependency(applicationName:"MyApp1", group:"org.springframework", name:"spring-expression", version:"4.0.5.RELEASE", configuration:"compile") 
+		assert dependencies[9] == new Dependency(applicationName:"MyApp1", group:"org.hamcrest", name:"hamcrest-core", version:"1.3", configuration:"test")
+		assert dependencies[10] == new Dependency(applicationName:"MyApp1", group:"org.hsqldb", name:"hsqldb", version:"2.3.2", configuration:"test")
 	}
 
-//	@Test
-//	void test_parse_FullGradleBuildFile_WithVariousMethodsAndProperties() {
-//		final SOURCE = '''
-//			final NEXUS_REPO = "http://some-nexus.acme.com:8081/nexus/content/repositories/releases"
-//			final BASE_NAME = 'SomeApplication'
-//			 
-//			// Configure repositories included from irt-common plugin
-//			ext.includeNexusReleasesRepository = true
-//			ext.includeNexusThirdPartyRepository = true
-//			 
-//			apply plugin: 'groovy'
-//			apply plugin: 'pmd'
-//			apply plugin: 'war'
-//			apply plugin: 'eclipse'
-//			 
-//			webAppDirName = 'WebContent'
-//			earBaseName = BASE_NAME + 'EAR'
-//			 
-//			sourceCompatibility = 1.8
-//			targetCompatibility = 1.8
-//			 
-//			// Artifactory Plugin
-//			buildscript {
-//			    repositories {
-//			        jcenter()                                   
-//			        maven { url "http://jcenter.bintray.com" }  
-//			    }
-//			    dependencies {
-//			        classpath "org.jfrog.buildinfo:build-info-extractor-gradle:4+"
-//			    }
-//			}
-//			 
-//			// Custom configuration to pull in extra jars
-//			configurations { warLibs }
-//			 
-//			dependencies {
-//			    compile group:'org.codehaus.groovy', name:'groovy-all', version:'2.3.9'
-//			    compile group: 'commons-codec', name: 'commons-codec', version: '1.6'
-//			    compile group: 'commons-configuration', name: 'commons-configuration', version: '1.6'
-//			    compile group: 'commons-lang', name: 'commons-lang', version: '2.4'
-//			    compile group: 'org.apache.httpcomponents', name: 'httpclient', version: '4.2.5'
-//			    compile group:'log4j', name:'log4j', version:'1.2.17'
-//			    compile group:'com.google.guava', name:'guava', version:'14.0.1'
-//			    compile group: 'org.commonjava.googlecode.markdown4j', name: 'markdown4j', version: '2.2-cj-1.1'
-//			    compile group: 'org.slf4j', name: 'slf4j-log4j12', version: '1.7.10'
-//			    compile group: 'javax.activation', name: 'activation', version: '1.1.1'
-//			    compile group: 'javax.validation', name: 'validation-api', version: '1.1.0.Final'
-//			    compile group: 'javax.annotation', name: 'javax.annotation-api', version: '1.2'
-//			    compile group: 'org.springframework', name: 'spring-web', version: '4.3.1.RELEASE'
-//			    compile group: 'org.javassist', name: 'javassist', version: '3.18.1-GA'
-//			    compile group: 'javax.ws.rs', name: 'javax.ws.rs-api', version: '2.0'
-//			    compile group: 'org.glassfish.hk2', name: 'spring-bridge', version: '2.5.0-b26', transitive:false
-//			    compile group: 'org.glassfish.jersey.ext', name: 'jersey-spring3', version: '2.7'
-//			                                                                                           
-//			    providedCompile group: 'javax.servlet', name: 'javax.servlet-api', version: '3.0.1'        
-//			    
-//			    testCompile group: 'junit', name: 'junit', version: '4.12'                                 
-//			    testCompile group: 'org.codenarc', name: 'CodeNarc', version: '0.27.0', transitive:false   
-//			    testCompile group: 'org.gmetrics', name: 'GMetrics', version: '0.7', transitive:false
-//			    testCompile group: 'org.apache.ant', name: 'ant', version: '1.8.4'                         
-//			    testCompile(group: 'org.eclipse.jetty.aggregate', name: 'jetty-all-server', version: '7.6.13.v20130916', transitive:false)
-//			    testCompile group: 'org.springframework', name: 'spring-test', version: '4.3.1.RELEASE'    
-//			    
-//			    warLibs group:'com.ibm', name:'somejar', version:'12.34.56'
-//			}
-//			 
-//			test {
-//			    include '**/SomeTestSuite.class'
-//			    jvmArgs '-Xmx1024m'
-//			}
-//			 
-//			war {
-//			    dependsOn createBuildVersionFile, createBuildTimestampFile
-//			   
-//			    baseName = BASE_NAME
-//			    classpath configurations.warLibs
-//			    classpath buildExtraDir
-//			}
-//			 
-//			pmd {
-//			    toolVersion = '4.2'
-//			    ruleSetFiles = files('Build/config/pmd/wystar-pmd-ruleset-strict.xml')   
-//			    ruleSets = []  // do not include default rulesets
-//			}
-//			 
-//			// Required for AHP builds (Build-Gradle.bat)
-//			task buildArtifacts(dependsOn: 'ear')
-//			 
-//			eclipse {
-//			    pathVariables 'GRADLE_IVY_REPO': file(System.getProperty("user.home") + '/.gradle/caches/modules-2/files-2.1')
-//			    // After you regenerate the Eclipse .classpath (gradle eclipseClasspath), then also:
-//			    //   * Remove groovy-all jar from the build path (because Eclipse includes its own Groovy support)
-//			    //   * Move the servlet-api:3.0.1 above GROOVY_SUPPORT in the build path.
-//			}
-//
-//			task createVersionFile doLast {
-//			    buildExtraDir.mkdirs()
-//			    versionFile.text = "abc"
-//			}
-//
-//			jar.dependsOn createVersionFile
-//			 
-//			javadoc {
-//			    title = 'MyProject'
-//			}
-//			 
-//			tasks.withType(FindBugs) {
-//			    reports {
-//			        xml.enabled = false
-//			        html.enabled = true
-//			    }
-//			}
-//
-//			// Code Generation Scripts ---------------------------------
-//			 
-//			buildscript {
-//			    final CODE_GEN_PLUGIN_VERSION = '0.6'
-//			    repositories { ivy { url "http://some-nexus.acme.com:8081/nexus/content/repositories/releases/" } }
-//			    dependencies {
-//			        classpath group:'Other', name:'code-generation', version:CODE_GEN_PLUGIN_VERSION, transitive:false
-//			    }
-//			}
-//			apply plugin: 'some-code-generation'
-//			 
-//			// Cobertura -----------------------------------------------
-//			 
-//			buildscript {
-//			    repositories {
-//			        ivy {
-//			            url "http://some-nexus.acme.com:8081/nexus/content/repositories/thirdparty/"
-//			        }
-//			    }
-//			    dependencies {
-//			        classpath("net.saliman:gradle-cobertura-plugin:2.3.2")
-//			    }
-//			}
-//			apply plugin: 'cobertura'
-//			 
-//			cobertura {
-//			    coverageSourceDirs = sourceSets.main.groovy.srcDirs
-//			    coverageExcludes = ['.*Test.class', '.*test.*', '.*Stub.*', '.*Mock.*']
-//			    coverageFormats = ['html', 'xml']
-//			}
-//			configurations {
-//			    // https://github.com/stevesaliman/gradle-cobertura-plugin/issues/41
-//			    cobertura.exclude group: 'xalan'
-//			    cobertura.exclude group: 'xerces'
-//			}
-//		'''
-//		assert parser.parse(NAME, SOURCE).size() == 25
-//	}
-//
-//	@Test
-//	void test_parse_UseOfGradleApi() {
-//		final SOURCE = """
-//			dependencies {
-//				compile "org.other:service:1.0:jdk15@jar"
-//			}
-//	
-//			boolean templateFileExists() {
-//			    FileCollection templateFiles = files(sourceSets.test.resources).filter { file -> file.name.toLowerCase().contains("jrxml") }
-//				return templateFiles
-//			}
-//		"""
-//		assert parser.parse(NAME, SOURCE).size() == 1
-//	}
-//
+	@Test
+	void test_parse_FullGrailsBuildConfigFile_2() {
+		String source = new File('src/test/resources/sample2-grails-buildconfig.txt').text
+		def dependencies = parser.parse(NAME, source)
+		assert dependencies.size() == 2
+		assert dependencies[0] == new Dependency(applicationName:"MyApp1", group:"mysql", name:"mysql-connector-java", version:"5.1.24", configuration:"runtime") 
+		assert dependencies[1] == new Dependency(applicationName:"MyApp1", group:"org.springframework.integration", name:"spring-integration-core", version:"2.2.5.RELEASE", configuration:"compile") 
+	}
+
 //	// Tests for other dependency options and formats
 //	
 //	@Test
@@ -356,35 +209,6 @@ class GrailsBuildConfigDependencyParserTest extends AbstractTestCase {
 //	}
 //	
 //	@Test
-//	void test_parse_SpecifyDependenciesInAVariable_List() {
-//		final SOURCE = """dependencies {
-//			List groovy = ["org.codehaus.groovy:groovy-all:2.4.10@jar",
-//			               "commons-cli:commons-cli:1.0@jar",
-//			               "org.apache.ant:ant:1.9.6@jar"]
-//			runtime groovy
-//		}"""
-//		assert parser.parse(NAME, SOURCE) == [
-//			new Dependency(applicationName:NAME, configuration:"runtime", group:"org.codehaus.groovy", name:"groovy-all", version:"2.4.10"),
-//			new Dependency(applicationName:NAME, configuration:"runtime", group:"commons-cli", name:"commons-cli", version:"1.0"),
-//			new Dependency(applicationName:NAME, configuration:"runtime", group:"org.apache.ant", name:"ant", version:"1.9.6"),
-//		]
-//	}
-//
-//	@Test
-//	void test_parse_SpecifyDependenciesInAVariable_MultipleLists() {
-//		final SOURCE = """dependencies {
-//			List groovy = ["org.codehaus.groovy:groovy-all:2.4.10@jar", "commons-cli:commons-cli:1.0@jar"]
-//			List hibernate = ['org.hibernate:hibernate:3.0.5@jar']		    
-//			runtime groovy, hibernate
-//		}"""
-//		assert parser.parse(NAME, SOURCE) == [
-//			new Dependency(applicationName:NAME, configuration:"runtime", group:"org.codehaus.groovy", name:"groovy-all", version:"2.4.10"),
-//			new Dependency(applicationName:NAME, configuration:"runtime", group:"commons-cli", name:"commons-cli", version:"1.0"),
-//			new Dependency(applicationName:NAME, configuration:"runtime", group:"org.hibernate", name:"hibernate", version:"3.0.5"),
-//		]
-//	}
-//
-//	@Test
 //	void test_parse_OtherSyntax() {
 //		final SOURCE = """dependencies {
 //			compile("org.gradle:api:1.0") {
@@ -409,21 +233,19 @@ class GrailsBuildConfigDependencyParserTest extends AbstractTestCase {
 	
 	// Tests for invalid DSL syntax/format
 	
-	
 	@Test
 	void test_parse_InvalidConfigurationName() {
 		shouldFail { parser.parse(NAME, "dependencies { custom 'log4j:log4:1.17' }") }
 	}
 
-	
-//	@Test
-//	void test_parse_StringFormat_Empty() {
-//		shouldFail { parser.parse(NAME, "dependencies { compile '' }") }	
-//	}
-//
-//	@Test
-//	void test_parse_IllegalGroovySyntax() {
-//		shouldFail(IllegalStateException) { parser.parse(NAME, "%^@ &*[]") }	
-//	}
+	@Test
+	void test_parse_StringFormat_Empty() {
+		final SOURCE = """grails.project.dependency.resolution = {
+			dependencies {
+				compile ""
+			}
+		}"""
+		shouldFailWithMessage("empty") { parser.parse(NAME, SOURCE) }
+	}
 	
 }
