2b6b05cd7cd148fabb7ead50760b7e26bad3fe75
Chris Mair <chrismair@sourceforge.net>
2017-07-16 19:33:50 -0400

    v0.5

--- src/main/java/org/dx42/needy/NeedyVersion.groovy
+++ src/main/java/org/dx42/needy/NeedyVersion.groovy
@@ -23,7 +23,7 @@ package org.dx42.needy
  */
 class NeedyVersion {
 
-	private static final String VERSION = "0.4"
+	private static final String VERSION = "0.5"
 	
 	@SuppressWarnings('GetterMethodCouldBeProperty')
 	static String getVersion() {
